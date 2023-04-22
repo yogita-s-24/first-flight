e5adf4916be711818d0ae864a2051af9f5b9969b
yogitashete2412 <yogitashete24@gmail.com>
2023-04-22 10:08:16 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 import ReactDOM from 'react-dom/client';
 
+import App from "./App";
+
+
+// function UserCard(){
+//     return(
+//         <>
+//         <h1>Hi I'm New Customer</h1>
+//         <button>Click Here to Register!</button>
+//         </>
+//     )
+// }
+
+// const student = "Ram";
+
+// const randomNumber = Math.random();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<> <h1>My First Flight🚀</h1> 
+ root.render(
+
+ //   <>
+
+//  <h1>My First Flight🚀</h1> 
+
+// {/* external variable */}
+//  <h2>Hello {student}</h2>
+
+//  {/* Random number generator */}
+//  <h2>Random Number = {randomNumber}</h2>
                 
-</>);
+//  <UserCard/> <UserCard/><UserCard/><UserCard/>
+// </>
+
+
+<>
+<App/>
+<App/>
+<App/>
+<App/>
+</>
+);
 
 
